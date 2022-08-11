0fcff5dd50325e4b725fcff6593a0f7fc372e966
JohnWCartmell <john.w.cartmell@gmail.com>
2022-08-11 22:50:29 +0100

    Update build.bat

--- diagramModelasERv1.4/build.bat
+++ diagramModelasERv1.4/build.bat
@@ -1,15 +1,16 @@
 echo %cd%
 call %~dp0\set_path_variables
 
-call %ERSCRIPTv1%/ERmodel_v1.4/scripts/gen_downgrade_to_v1.3.bat diagramERmodel.xml
+SET SUBJECTNAMEBASE=diagramERmodel
+CALL %ERSCRIPTv1%/ERmodel_v1.4/scripts/gen_downgrade_to_v1.3.bat %SUBJECTNAMEBASE%.xml
 
 call %ERSCRIPTv1%/ERmodel_v1.4/scripts/set_path_variables
 
-java -jar %SAXON_PATH%\saxon9he.jar -s:..\temp\%filenamebase%.xml -xsl:%ERHOME%\xslt\ERmodel2.physical.xslt -o:..\temp\%filenamebase%.hierarchical.xml style=hs
+java -jar %SAXON_PATH%\saxon9he.jar -s:..\temp\%SUBJECTNAMEBASE%.xml -xsl:%ERHOME%\xslt\ERmodel2.physical.xslt -o:..\temp\%SUBJECTNAMEBASE%.hierarchical.xml style=hs
 
-java -jar %SAXON_PATH%\saxon9he.jar -s:..\temp\%filenamebase%.hierarchical.xml -xsl:%ERHOME%\xslt\ERmodel2.svg.xslt -o:..\docs\%filenamebase%.hierarchical.svg filestem=%filenamebase%.hierarchical
+java -jar %SAXON_PATH%\saxon9he.jar -s:..\temp\%SUBJECTNAMEBASE%.hierarchical.xml -xsl:%ERHOME%\xslt\ERmodel2.svg.xslt -o:..\docs\%SUBJECTNAMEBASE%.hierarchical.svg filestem=%filenamebase%.hierarchical
 
 
 echo FINALLY NEED TO GENERATE rng
 
-java -jar %SAXON_PATH%\saxon9he.jar -s:../temp/diagramERModel.hierarchical.xml -xsl:%ERSCRIPTv1%/ERmodel_v1.4/xslt/ERmodel2.rng.xslt -o:../bin/diagram.rng 
+java -jar %SAXON_PATH%\saxon9he.jar -s:../temp/%SUBJECTNAMEBASE%.hierarchical.xml -xsl:%ERSCRIPTv1%/ERmodel_v1.4/xslt/ERmodel2.rng.xslt -o:../bin/diagram.rng 
